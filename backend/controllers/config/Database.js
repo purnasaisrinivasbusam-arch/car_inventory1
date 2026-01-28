0be4d9f65aa4173f81ec8352add2906972d93f65
unknown <purnasaisrinivasbusam@gmail.com>
2026-01-28 15:06:41 +0530

    database cluster change

--- backend/controllers/config/Database.js
+++ backend/controllers/config/Database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://bharath:bharath@cluster0.mcnlrgp.mongodb.net/?appName=Cluster0"
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://srinivas:srinivas@cluster0.mab8vmo.mongodb.net/?appName=Cluster0"
 
 // Establish connection to MongoDB cluster
 const atlasConnection = mongoose.createConnection(MONGO_URI, {
